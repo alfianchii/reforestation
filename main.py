8af8194c1df3c6cddf6d91ff182d08f60d2c9d38
alfianchii <pianaee379@gmail.com>
2023-03-07 12:35:58 +0700

    feat: add a snake

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+def makeCommits (days : int):
+    if days < 1:
+        os.system('git push')
+    else:
+        dates = f"{days} days ago"
+        with open('LAST_UPDATED', 'a') as file:
+            file.write(f'{dates} <- this was the commit for the day!!\n')
+        
+        # staging 
+        os.system('git add LAST_UPDATED')
+
+        # commit 
+        os.system('git commit --date="'+ dates +'" -m "There was nothing here."')
+
+        return days * makeCommits(days - 1)
+
+makeCommits(3)
