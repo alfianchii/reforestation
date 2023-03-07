3d019c25b3a5430a66c41dc439dedb710665ef06
alfianchii <pianaee379@gmail.com>
2023-03-07 12:45:43 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import os
+from datetime import datetime, timezone, timedelta
 
-def makeCommits (days : int):
+def makeCommits(days: int) -> int:
     if days < 1:
         os.system('git push')
+        return 0
     else:
-        dates = f"{days} days ago"
-        with open('LAST_UPDATED', 'a') as file:
-            file.write(f'{dates} <- this was the commit for the day!\n')
+        # Get the current datetime in UTC
+        now_utc = datetime.now(timezone.utc)
+        # Subtract days from the current datetime
+        commit_date = now_utc - timedelta(days=days)
+        # Format the date in ISO 8601 format
+        date_str = commit_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+        
+        with open('LAST_UPDATED', 'w') as file:
+            file.write(date_str)
         
         # staging 
         os.system('git add LAST_UPDATED')
 
         # commit 
-        os.system('git commit --date="'+ dates +'" -m "There was nothing here."')
+        os.system(f'git commit --date="{date_str}" -m "There was nothing here."')
 
         return days * makeCommits(days - 1)
 
-makeCommits(3)
+makeCommits(1)
